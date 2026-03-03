6293beacd784a39772db9dca777c5e6a13bca293
Marins <jmarinsti@gmail.com>
2026-03-03 10:24:54 -0300

    Criados as classes Order, OrderStatus e User, além da configuração do H2.

---
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Order.java
@@ -0,0 +1,33 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+
+@Entity
+@Table(name="tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+}
