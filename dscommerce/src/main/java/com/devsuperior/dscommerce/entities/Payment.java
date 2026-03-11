e178d8d93a9cc408fb474f1cba9034f3b0c6342c
Marins <jmarinsti@gmail.com>
2026-03-11 09:17:11 -0300

    Equals hashCode

--- dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Payment.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Payment.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
 @Table(name="tb_payment")
@@ -45,5 +46,18 @@ public class Payment {
 
     public void setOrder(Order order) {
         this.order = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Payment payment = (Payment) o;
+        return Objects.equals(Id, payment.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(Id);
     }
 }
