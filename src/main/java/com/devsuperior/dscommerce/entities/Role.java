3869a7524044e0f1a7c20289d6e04dc0a9390db2
Marins <jmarinsti@gmail.com>
2026-05-19 09:24:18 -0300

    UserDetails, Granted Authority, not tested

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,21 +1,22 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long authority;
+    private String authority;
 
     public Role() {
 
     }
 
-    public Role(Long id, Long authority) {
+    public Role(Long id, String authority) {
         this.id = id;
         this.authority = authority;
     }
@@ -28,11 +29,12 @@ public class Role {
         this.id = id;
     }
 
-    public Long getAuthority() {
+    @Override
+    public String getAuthority() {
         return authority;
     }
 
-    public void setAuthority(Long authority) {
+    public void setAuthority(String authority) {
         this.authority = authority;
     }
 
